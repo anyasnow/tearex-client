cd40ec0261ddab4fc98eecd9485593426423777e
Anya Snow <emailasnow@gmail.com>
2019-06-11 17:22:26 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
+}
+export default App;
